2deaf6431d0313a8f136a2a8a58dd7c3f20e9471
lgshogun <lauro.gpereira@gmail.com>
2023-09-28 15:57:50 -0300

    :tada: Criação do Bundle Springboot

---
+++ src/test/java/com/example/campeonatoapi/CampeonatoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campeonatoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampeonatoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
